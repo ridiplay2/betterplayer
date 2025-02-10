fbce359d01c4c2894c164bcc95ba2e8a85f69a55
Jihun Jang <jhjang@ridi.com>
2025-02-10 18:06:24 +0900

    add debug log

--- ios/Classes/BetterPlayerPlugin.m
+++ ios/Classes/BetterPlayerPlugin.m
@@ -356,6 +356,8 @@ bool _remoteCommandsInitialized = false;
             }
             
             if (startPositionMs != NULL) {
+                NSLog(@"BetterPlayer startPositionMs is not null, seek to its value: %d", [startPositionMs intValue]);
+
                 [player seekTo:[startPositionMs intValue] withCompletionHandler:^(BOOL finished) {
                     result(nil);
                 }];
